02952c5b6a4f6923dffa382bd16fe06e2d1c3596
Hugo Corrêa <hugodemorais@live.com>
2021-10-02 21:35:58 -0300

    att

--- main.js
+++ main.js
@@ -5,58 +5,58 @@ console.log(keys);
 // play notes
 function playNote(event) {
   // keyCode
-  let audioKeyCode = getKeyCode(event)
+  let audioKeyCode = getKeyCode(event);
 
   // typed or pressed key
-  const keyTyped = document.querySelector(`.key[data-key="${audioKeyCode}"`)
+  const keyTyped = document.querySelector(`.key[data-key="${audioKeyCode}"`);
 
   // if key exists
-  const cantFoundAnyKey = !keyTyped
+  const cantFoundAnyKey = !keyTyped;
 
-  if(cantFoundAnyKey){
-      return
+  if (cantFoundAnyKey) {
+    return;
   }
 
-  playAudio(audioKeyCode)
-  addPlayingClass(keyTyped)
-
+  playAudio(audioKeyCode);
+  addPlayingClass(keyTyped);
 }
 
-function addPlayingClass(key){
-    key.classList.add('playing')
-
+function addPlayingClass(key) {
+  key.classList.add("playing");
 }
 
-function removePlayingClass(event){
-    event.target.classList.remove('playing')
+function removePlayingClass(event) {
+  event.target.classList.remove("playing");
 }
 
-function getKeyCode(event){
-    let keyCode;
+function getKeyCode(event) {
+  let keyCode;
 
-    const isKeyboard = event.type === "keydown"; // true or false
-    if (isKeyboard) {
-        keyCode = event.keyCode
-    } else {
-        keyCode = event.target.dataset.key
-  
-    }
+  const isKeyboard = event.type === "keydown"; // true or false
+  if (isKeyboard) {
+    keyCode = event.keyCode;
+  } else {
+    keyCode = event.target.dataset.key;
+  }
 
-    return keyCode
+  return keyCode;
 }
 
-function playAudio(audioKeyCode){
-    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"`)
-    audio.currentTime = 0
-    audio.play()
+function playAudio(audioKeyCode) {
+  const audio = document.querySelector(`audio[data-key="${audioKeyCode}"`);
+  audio.currentTime = 0;
+  audio.play();
 }
 
-// click with mouse
-keys.forEach(function (key) {
-  key.addEventListener("click", playNote);
-  key.addEventListener("transitionend", removePlayingClass);
+function registerEvents() {
+  // click with mouse
+  keys.forEach(function (key) {
+    key.addEventListener("click", playNote);
+    key.addEventListener("transitionend", removePlayingClass);
+  });
 
-});
+  // keyboard type
+  window.addEventListener("keydown", playNote);
+}
 
-// keyboard type
-window.addEventListener("keydown", playNote);
+window.addEventListener("load", registerEvents)
