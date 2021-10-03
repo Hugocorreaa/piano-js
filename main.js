d195b87de662201ca565f6f301c1530fc888fb8c
Hugo Corrêa <hugodemorais@live.com>
2021-10-02 21:24:47 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+// get all keys
+const keys = document.querySelectorAll(".key");
+console.log(keys);
+
+// play notes
+function playNote(event) {
+  // keyCode
+  let audioKeyCode = getKeyCode(event)
+
+  // typed or pressed key
+  const keyTyped = document.querySelector(`.key[data-key="${audioKeyCode}"`)
+
+  // if key exists
+  const cantFoundAnyKey = !keyTyped
+
+  if(cantFoundAnyKey){
+      return
+  }
+
+  playAudio(audioKeyCode)
+  addPlayingClass(keyTyped)
+
+}
+
+function addPlayingClass(key){
+    key.classList.add('playing')
+
+}
+
+function removePlayingClass(event){
+    event.target.classList.remove('playing')
+}
+
+function getKeyCode(event){
+    let keyCode;
+
+    const isKeyboard = event.type === "keydown"; // true or false
+    if (isKeyboard) {
+        keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+  
+    }
+
+    return keyCode
+}
+
+function playAudio(audioKeyCode){
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"`)
+    audio.currentTime = 0
+    audio.play()
+}
+
+// click with mouse
+keys.forEach(function (key) {
+  key.addEventListener("click", playNote);
+  key.addEventListener("transitionend", removePlayingClass);
+
+});
+
+// keyboard type
+window.addEventListener("keydown", playNote);
